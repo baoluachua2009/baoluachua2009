42a6cb450e4a655738cf404ebb700668dc436c06
Nguyễn Gia Bảo <125172032+baoluachua2009@users.noreply.github.com>
2023-05-24 09:44:50 +0700

    Update Script Online.lua

--- Script Online.lua
+++ Script Online.lua
@@ -1,11 +1,73 @@
-JScriptCodeDom.CodeParseException:  Unexpected Char : ':'  , Line 1, Char 29 ---> System.Exception:  Unexpected Char : ':' 
-   at JScriptCodeDom.CodeParser.InternalParseStatementCore()
-   at JScriptCodeDom.CodeParser.InternalParseStatement()
-   at JScriptCodeDom.CodeParser.ParseStatement()
-   at JScriptCodeDom.CodeParser.ParseBlock()
-   at JScriptCodeDom.CodeParser.Parse(String code, String filename, Int32 lineoffset, Boolean keeplinenum)
-   --- End of inner exception stack trace ---
-   at JScriptCodeDom.CodeParser.Parse(String code, String filename, Int32 lineoffset, Boolean keeplinenum)
-   at ProtectorV1.Protect(String[] codes)
-   at ProtectorV1.Protect(String code)
-   at ASP.javascript_obfuscator_aspx.Button1_Click(Object sender, EventArgs e)
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Cay Chua Haha";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 5;game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Cay Chua Haha";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 6;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Cay Chua Haha";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 7;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Cay Chua Haha";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 8;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Cay Chua Haha";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 9;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Cay Chua Haha";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 10;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Cay Chua Haha";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 11;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Cay Chua Haha";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 12;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Ngu Như Con Chó Script Troll By Bảo Lửa Chùa";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 13;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Ngu Như Con Chó Script Troll By Bảo Lửa Chùa";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 14;
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Script Troll By baoluachua";
+	Text = "Ngu Như Con Chó Script Troll By Bảo Lửa Chùa";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 15;
+game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport", "13623c48-cb2d-431d-b848-e73d1b90ae43")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
+if _G.wl_key then
+    return
+end
+getgenv().Team = "Pirates"
+--Do not execute any scripts before running the script
+--Put Your Key Between "" at the _G.wl_key
+_G.wl_key = ""
+repeat wait()
+until game:IsLoaded()
+delay(DelayTime or 300,function()
+    local CG = game:GetService("CoreGui")
+    if not CG:FindFirstChild("W-azure") then
+       game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+    end
+end)
+wait(2)
+loadstring(game:HttpGet('https://scripts.luawl.com/hosted/1989/15063/Main-rewrite.lua'))()
